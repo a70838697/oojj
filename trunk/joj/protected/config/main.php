dbed0a0759eecf4686c310e655985e53dc675d54
casper <casper@0afd6cab-e0e8-8548-8c89-35825b1e705c>
2011-02-20 09:24:43 +0000

    git-svn-id: https://jnu21701/svn/jnu@32 0afd6cab-e0e8-8548-8c89-35825b1e705c

--- trunk/joj/protected/config/main.php
+++ trunk/joj/protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'OJJ: Online Judge of JNU',
+	'name'=>'OOJJ: Open Online Judge of JNU',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
