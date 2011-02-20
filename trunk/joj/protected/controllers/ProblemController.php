cc68369c91a2b51791b965eb1ab6a4f2371c40b5
casper <casper@0afd6cab-e0e8-8548-8c89-35825b1e705c>
2011-02-20 12:26:06 +0000

    git-svn-id: https://jnu21701/svn/jnu@33 0afd6cab-e0e8-8548-8c89-35825b1e705c

--- trunk/joj/protected/controllers/ProblemController.php
+++ trunk/joj/protected/controllers/ProblemController.php
@@ -7,7 +7,7 @@ class ProblemController extends Controller
 	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
 	 * using two-column layout. See 'protected/views/layouts/column2.php'.
 	 */
-	public $layout='//layouts/column1';
+	public $layout='//layouts/onlinejudge';
 	public $contentMenu=null;
 
 	/**
