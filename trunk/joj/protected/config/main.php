41959754b760487f261bf755c428c6406fb36f4e
casper <casper@0afd6cab-e0e8-8548-8c89-35825b1e705c>
2011-02-21 14:05:44 +0000

    git-svn-id: https://jnu21701/svn/jnu@38 0afd6cab-e0e8-8548-8c89-35825b1e705c

--- trunk/joj/protected/config/main.php
+++ trunk/joj/protected/config/main.php
@@ -131,6 +131,6 @@ return array(
 	// using Yii::app()->params['paramName']
 	'params'=>array(
 		// this is used in contact page
-		'adminEmail'=>'webmaster@example.com',
+		'adminEmail'=>'worthful@qq.com',
 	),
 );
