3efab8a3d1c8d065af461e9d11294eacc0c51624
casper <casper@0afd6cab-e0e8-8548-8c89-35825b1e705c>
2011-02-21 14:21:23 +0000

    git-svn-id: https://jnu21701/svn/jnu@39 0afd6cab-e0e8-8548-8c89-35825b1e705c

--- trunk/joj/protected/modules/user/controllers/ActivationController.php
+++ trunk/joj/protected/modules/user/controllers/ActivationController.php
@@ -19,6 +19,9 @@ class ActivationController extends Controller
 				$find->activkey = UserModule::encrypting(microtime());
 				$find->status = 1;
 				$find->save();
+				$find->profile->group=10;
+				$find->profile->save();
+				
 			    $this->render('/user/message',array('title'=>UserModule::t("User activation"),'content'=>UserModule::t("You account is activated.")));
 			} else {
 			    $this->render('/user/message',array('title'=>UserModule::t("User activation"),'content'=>UserModule::t("Incorrect activation URL.")));
