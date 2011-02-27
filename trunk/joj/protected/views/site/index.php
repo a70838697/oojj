e6e089f03fa0d3248e2f8fb593ea40dd47e4d61f
casper <casper@0afd6cab-e0e8-8548-8c89-35825b1e705c>
2011-02-27 08:51:32 +0000

    git-svn-id: https://jnu21701/svn/jnu@48 0afd6cab-e0e8-8548-8c89-35825b1e705c

--- trunk/joj/protected/views/site/index.php
+++ trunk/joj/protected/views/site/index.php
@@ -1,8 +1,22 @@
 <?php $this->pageTitle=Yii::app()->name; ?>
 
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+<font color=red>请暨大注册的同学注意：注册时候请填写自己的真实姓名和邮箱，注册的过程：</font><br/>
+1.填写注册资料。<br/>
+2.从邮箱中查找激活链接，有时候在你的垃圾文件夹里。<br/>
+3.激活帐号。<br/>
+4.登录，在你的Profiles里面有个链接 become a jnuer.<br/>
+5.点击并填写资料，注意填写自己准确的学号，姓名，专业，等待管理员核实你的资料。<br/>
+6.一旦管理员核准，重新登录后就可以看到course选项，可以选课，等待老师批准你的选课。<br/>
+7.做实验。
+<br/>
+
+
+<br/>
 
 The system is still being developed.<br/>
 But without doubt, you can submit your solutions to the problems.
 <br/>
 Wish you great success in AC.
+
+<br/>
