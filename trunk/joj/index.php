7ed939bafbcc0dfce7a30757d3c4f147937b6723
casper <casper@0afd6cab-e0e8-8548-8c89-35825b1e705c>
2011-03-02 09:30:51 +0000

    git-svn-id: https://jnu21701/svn/jnu@61 0afd6cab-e0e8-8548-8c89-35825b1e705c

--- trunk/joj/index.php
+++ trunk/joj/index.php
@@ -1,5 +1,5 @@
 <?php
-
+header('Content-type:text/html;charset=utf-8');
 // change the following paths if necessary
 $yii=dirname(__FILE__).'/../yii/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
